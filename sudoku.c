5d4b734f639fa325b900417f640953f376c6984c
Philip Puryear <philippuryear@gmail.com>
2014-03-27 02:13:02 -0500

    Bump copyright year.

--- sudoku.c
+++ sudoku.c
@@ -1,4 +1,4 @@
-// Copyright 2012 Philip Puryear
+// Copyright 2014 Philip Puryear
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
